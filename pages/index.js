9c780d6160d894cff6d98983033a975cdfad40a8
atilaC0D3 <124696990+atilac0d3@users.noreply.github.com>
2023-06-19 23:19:17 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Amor... Veja se funcionou aí... Eu só te amo!!! Se você tb me ama manda uma piscadinha.</h1>
+    return <h1>Amor... Veja se funcionou aí... Eu só te amo!!! Se você tb me ama manda uma piscadinha. 😉😘 !!! </h1>
 }
 
 export default Home;
