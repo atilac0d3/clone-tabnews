799c6e30a1ea7429eb8d05a5f981341e7c9e987d
atilaC0D3 <124696990+atilac0d3@users.noreply.github.com>
2023-07-19 00:55:09 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Amor... Veja se funcionou aí... Eu só te amo!!! Se você tb me ama manda uma piscadinha. 😉😘 !!! </h1>
+  return (
+    <h1>
+      Amor... Veja se funcionou aí... Eu só te amo!!! Se você tb me ama manda
+      uma piscadinha. 😉😘 !!!{" "}
+    </h1>
+  );
 }
 
 export default Home;
